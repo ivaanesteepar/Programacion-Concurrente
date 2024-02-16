67e4f4d5a92f43bd216a0e06c0fd1d690761ae23
Iván Estépar <102294523+ivaanesteepar@users.noreply.github.com>
2024-02-16 11:53:56 +0100

    Update HiloO.java

--- Practica1/p04/HiloO.java
+++ Practica1/p04/HiloO.java
@@ -4,18 +4,17 @@ public class HiloO implements Runnable{
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		for (int i = 0; i < 100; i++) {
-			System.out.print("o");
+	// TODO Auto-generated method stub
+	for (int i = 0; i < 100; i++) {
+		System.out.print("o");
 			
-			try {
-				Thread.sleep(2);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			Thread.sleep(2);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		}
 		System.out.println();
 	}
-
 }
