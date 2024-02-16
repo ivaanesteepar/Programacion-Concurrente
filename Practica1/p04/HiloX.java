76522d98e54346f10e232b67da6c713db5094907
Iván Estépar <102294523+ivaanesteepar@users.noreply.github.com>
2024-02-16 11:54:47 +0100

    Update HiloX.java

--- Practica1/p04/HiloX.java
+++ Practica1/p04/HiloX.java
@@ -4,18 +4,17 @@ public class HiloX implements Runnable{
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		for (int i = 0; i < 100; i++) {
-			System.out.print("x");
+	// TODO Auto-generated method stub
+	for (int i = 0; i < 100; i++) {
+		System.out.print("x");
 			
-			try {
-				Thread.sleep(2);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			Thread.sleep(2);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		}
 		System.out.println();
 	}
-
 }
