48d105e0f1b09228b29a53ab1139952c3b43d5a8
Iván Estépar <102294523+ivaanesteepar@users.noreply.github.com>
2024-02-19 11:00:04 +0100

    Update CasoEstudio2.java

--- Practica1/p02/CasoEstudio2.java
+++ Practica1/p02/CasoEstudio2.java
@@ -9,17 +9,17 @@ public class CasoEstudio2 {
 		Thread hilo_ = new Thread(new Hilo_());
 		Thread hiloO = new Thread(new HiloO());
 		
-		hilox.start(); //Se ejecuta el hilo de las x
+		hilox.start(); // Se ejecuta el hilo de las x
 
 		try {
-			hilox.join(); //Hasta que no acabe el hilo de las x, no se ejecutará lo siguiente
+			hilox.join(); // Hasta que no acabe el hilo de las x, no se ejecutará lo siguiente
 			
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		//Una vez acabado el hilo de las x, se ejecuta esto
+		// Una vez acabado el hilo de las x, se ejecuta esto
 		System.out.print("Fin hilo x");
 		hilo_.start();
 		hiloO.start();
