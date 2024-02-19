6f13358985a668299aa94718d0d410f63e3b4b15
Iván Estépar <102294523+ivaanesteepar@users.noreply.github.com>
2024-02-19 18:18:36 +0100

    Update CasoEstudio3.java

--- Practica1/p03/CasoEstudio3.java
+++ Practica1/p03/CasoEstudio3.java
@@ -1,30 +1,21 @@
 package p03;
-import java.util.Scanner;
 
 public class CasoEstudio3 {
 
     public static void main(String[] args) {
-        // Solicitar entrada del usuario para las variables del hilo
-        Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Ingrese el carácter: ");
-        char caracter = scanner.next().charAt(0);
-
-        System.out.print("Ingrese el tiempo entre escrituras (en milisegundos): ");
-        int tiempoEntreEscrituras = scanner.nextInt();
-
-        System.out.print("Ingrese el número de repeticiones: ");
-        int repeticiones = scanner.nextInt();
-
-        // Crear un único tipo de hilo con tres parámetros
-        Thread hiloParametrizado = new Thread(new Hilo_(caracter, tiempoEntreEscrituras, repeticiones));
-
-        hiloParametrizado.setPriority(Thread.MAX_PRIORITY);
+        Thread hilox = new Thread(new Hilo('x', 1, 4));
+        Thread hilo_ = new Thread(new Hilo('-', 2, 2));
+        Thread hiloO = new Thread(new Hilo('o', 3, 2));
+                
+        hilox.start();
+        hilo_.start();
+        hiloO.start();
         
-        hiloParametrizado.start();
+        hiloO.setPriority(Thread.MAX_PRIORITY);
 
         try {
-            hiloParametrizado.join();
+            hilox.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -32,30 +23,4 @@ public class CasoEstudio3 {
         System.out.print("Fin hilo parametrizado");
     }
 
-}
-
-class HiloParametrizado implements Runnable {
-
-    private char caracter;
-    private int tiempoEntreEscrituras;
-    private int repeticiones;
-
-    public HiloParametrizado(char caracter, int tiempoEntreEscrituras, int repeticiones) {
-        this.caracter = caracter;
-        this.tiempoEntreEscrituras = tiempoEntreEscrituras;
-        this.repeticiones = repeticiones;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < repeticiones; i++) {
-            System.out.print(caracter);
-
-            try {
-                Thread.sleep(tiempoEntreEscrituras);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
